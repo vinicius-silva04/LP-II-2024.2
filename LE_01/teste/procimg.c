64a6d14288cc731b424e46cd0b948a913d303ad7
vinicius-silva04 <vjsilva.cic@uesc.br>
2024-08-29 15:46:50 -0300

    Create procimg.c

---
+++ LE_01/teste/procimg.c
@@ -0,0 +1,30 @@
+//Q1
+unsigned char gerarGreyPixel (int tipo){
+    int valor;
+    int proba= rand() % 100; //gera um número entre 0 e 100;
+    if(tipo<0){
+       if(proba < 80){      //80% de gerar um número entre 0 e 127.
+            valor= rand() % 128;
+       }else{               //20% de gerar um número de qualquer intensidade.
+            valor= rand() % 256;
+        }
+    }else if(tipo>0){
+        if(proba < 80){     //80% de gerar um número entre 128 e 255.
+            valor = 128 + rand() % 128;
+        }else{
+            valor = rand() % 256; //20% de gerar um número de qualquer intensidade.
+        }
+    }else{
+        valor = rand() % 256; //gera um número entre 0 e 255.
+    }
+return (unsigned char) valor;
+}
+//Q2
+void geraImgGreyFull ( unsigned char img [640][480], unsigned char pixel){
+    int i,j;
+    for(i=0;i<640;i++){
+        for(j=0;j<480;j++){
+            img[i][j]= pixel;
+        }
+    }
+}
