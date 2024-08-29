77a70f89113dc6d696fcbd13a5339950508e863a
Guilherme Afonso Bongestab <gbongestab@gmail.com>
2024-08-29 19:54:30 -0300

    Update LP2-lista1-3.c

--- LP2-lista1-3.c
+++ LP2-lista1-3.c
@@ -1,37 +1,23 @@
-//Implemente uma a função void geraImgGreyB(unsigned char img[640][480]), que preenche todos os pixels de uma imagem com o valor 0 (black, intensidade zero em imagem cinza significa cor preta).
+//Implemente uma a funÃ§Ã£o void geraImgGreyB(unsigned char img[640][480]), que preenche todos os pixels de uma imagem com o valor 0 (black, intensidade zero em imagem cinza significa cor preta).
 
 #include <stdio.h>
 #include <stdlib.h>
-
-//cabeçalho da função
-void geraImgGreyB(unsigned char img[640][480]);
-
+#include "procimg.h"
 
 int main(){
     int i, j;
     unsigned char img[640][480];
 
-    geraImgGreyB(img);                       //Chamada da função
+    geraImgGreyB(img);                       //Chamada da funÃ§Ã£o de procimg.h
 
     for(i = 0; i < 640; i++){
         for(j = 0; j < 480; j++){
-            printf("%u", img[i][j]);         //Exibição da matriz
+            printf("%u", img[i][j]);         //ExibiÃ§Ã£o da matriz
         }
         printf("\n");
     }
 
-    printf("\n\nTodos os elementos são 0.");
+    printf("\n\nTodos os elementos sÃ£o 0.");
 
     return 0;
-}
-
-//função preenche todos os pixels com 0
-void geraImgGreyB(unsigned char img[640][480]){
-    int i, j;
-
-    for(i = 0; i < 640; i++){
-        for(j = 0; j < 480; j++){
-            img[i][j] = 0;
-        }
-    }
 }
