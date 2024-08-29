cf1224f0cca2a0142717fb846603735787bb189d
vinicius-silva04 <vjsilva.cic@uesc.br>
2024-08-29 15:45:43 -0300

    Create procimg.h

---
+++ LE_01/teste/procimg.h
@@ -0,0 +1,19 @@
+//Q1
+unsigned char gerarGreyPixel (int tipo);
+/*
+    *Função que gera um número que determina a intensidade de cor do pixel.
+    *Parâmetro:
+    * tipo: número que determina qual intensidade escolhida.
+    *Retorno:
+    * int: número gerado que determina a intensidade.
+*/
+//Q2
+void geraImgGreyFull ( unsigned char img [640][480], unsigned char pixel);
+/*
+    *Função que preenche a imagem com o valor do parâmetro pixel.
+    *Parâmetro:
+    * img: matriz onde a imagem será preenchida.
+    * pixel: número que será preenchido a imagem.
+    *Retorno:
+    * int: número gerado que determina a intensidade.
+*/
