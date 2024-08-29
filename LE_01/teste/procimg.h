6e3fa424c100f9ba6785fe82cb9f852196de55e8
Cauan Mendonça Valadão <167363904+CauanValadao@users.noreply.github.com>
2024-08-29 20:42:07 -0300

    Update procimg.h

--- LE_01/teste/procimg.h
+++ LE_01/teste/procimg.h
@@ -29,6 +29,16 @@ void drwRect(unsigned char img[640][480], int x, int y, int larg, int alt, unsig
     *x,y: coordenadas do vertice superior esquerdo do retangulo
     *larg,alt: largura e altura do retangulo
 */
+//Q7
+void drwCirc(unsigned char img[tamanho][tamanho], int x, int y, int rad, unsigned char pixel);
+/*
+   *Função que desenha a área de um círculo.
+   *Parâmetros:
+   *img: matriz onde o círculo será desenhado.
+   *x,y: coordenadas do centro do círculo.
+   *rad: raio da circunferência.
+   *pixel: cor que será usada.
+*/
 //Q12
 void imgHistogram(unsigned char img[640][480], int hist[256]);
 /*
