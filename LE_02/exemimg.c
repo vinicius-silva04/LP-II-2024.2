658b3c44d477d3e786be61c0073136ae069266da
Cauan Mendonça Valadão <167363904+CauanValadao@users.noreply.github.com>
2024-09-05 19:08:59 -0300

    Update exemimg.c

--- LE_02/exemimg.c
+++ LE_02/exemimg.c
@@ -27,3 +27,29 @@ int main(){
     return 0;
 }
 */
+//Q3
+/*#include <stdio.h>
+#define linha 480
+#define coluna 640
+int main(){
+    unsigned char img[linha][coluna];
+    int cont = 0;
+   
+    geraImgGreyB_R(img, linha, coluna);
+    
+    for(int i = 0; i < linha; i++){                                         // Verifica se a imagem foi gerada corretamente
+        for(int j = 0; j < coluna; j++){ 
+            if(img[i][j] != 0){
+                printf("Erro: img[%d][%d] = %hhu\n", i, j, img[i][j]);          // Se algum pixel não for igual a 0, a imagem não foi gerada corretamente
+            }
+            else cont++;                                        // Conta a quantidade de pixels gerados iguais a 0        
+        }
+    }
+
+    if(linha * coluna == cont) printf("\n\nImagem gerada com sucesso!\n");     //linha * coluna representa a quantidade de pixels da imagem, que deve ser igual a cont
+    else printf("\n\nErro na geracao da imagem!\n");
+
+    return 0;
+}
+*/
+
