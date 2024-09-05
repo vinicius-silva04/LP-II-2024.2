2603f7e78001ec08598462bb40ef0d5a44a316af
Cauan Mendonça Valadão <167363904+CauanValadao@users.noreply.github.com>
2024-09-05 18:27:29 -0300

    questao 2

--- LE_02/procimg.h
+++ LE_02/procimg.h
@@ -1,1 +1,9 @@
-
+//Q2
+void geraImgGreyFull_R(unsigned char img[linha][coluna], unsigned char pixel, int i, int j);
+/*
+    *Função que preenche uma imagem 640x480 com pixel utilizando de recursividade
+    *Parâmetro:
+    * img: matriz onde a imagem será preenchida.
+    *pixel: número que será preenchido a imagem.
+    *i e j: variáveis para iterar entre as linhas e colunas da matriz
+*/
