5b1096ac7c517add926270f2a432b3af231869b4
Cauan Mendonça Valadão <167363904+CauanValadao@users.noreply.github.com>
2024-09-05 18:33:33 -0300

    questao 2-cauan

--- LE_02/exemimg.c
+++ LE_02/exemimg.c
@@ -1,1 +1,29 @@
+//Q2
+/*#include <stdio.h>
+#define linha 480
+#define coluna 640
+int main(){
+    unsigned char img[linha][coluna], pixel;
+    int cont = 0;
 
+    printf("Digite o valor do pixel: ");
+    scanf("%hhu", &pixel);
+    
+    geraImgGreyFull_R(img, pixel, linha, coluna);
+    
+    for(int i = 0; i < linha; i++){                                         // Verifica se a imagem foi gerada corretamente
+        for(int j = 0; j < coluna; j++){ 
+            if(img[i][j] != pixel){
+                printf("Erro: img[%d][%d] = %hhu\n", i, j, img[i][j]);          // Se algum pixel não for igual ao valor digitado, a imagem não foi gerada corretamente
+                return 0;
+            }
+            else cont++;                                        // Conta a quantidade de pixels gerados iguais ao valor digitado         
+        }
+    }
+
+    if(linha * coluna == cont) printf("\n\nImagem gerada com sucesso!\n");     //linha * coluna representa a quantidade de pixels da imagem, que deve ser igual a cont
+    else printf("\n\nErro na geração da imagem!\n");
+
+    return 0;
+}
+*/
