290ea86453800975721c53fdc50c5ddd164ed0f9
Cauan Mendonça Valadão <167363904+CauanValadao@users.noreply.github.com>
2024-09-05 19:10:23 -0300

    Update procimg.c

--- LE_02/procimg.c
+++ LE_02/procimg.c
@@ -5,3 +5,10 @@ void geraImgGreyFull_R(unsigned char img[linha][coluna], unsigned char pixel, in
     if(j == 0) geraImgGreyFull_R(img, pixel, i-1, coluna);  //quando apenas j for 0, a linha i ja tera sido preenchida, e passamos para a proxima linha i-1, e voltamos para a primeira coluna->coluna
     else geraImgGreyFull_R(img, pixel, i, j-1); //quando j for diferente de 0, preenchemos a mesma linha i, e passamos para a coluna j-1
 }
+//Q3
+void geraImgGreyB_R(unsigned char img[linha][coluna],int i ,int j){
+    if(i == 0 && j == 0) return;    //quando i e j forem iguais a 0, a imagem estará completa, e a função encerra
+    img[i-1][j-1] = 0;          //atribui o valor do pixel na posição i-1 e j-1
+    if(j == 0) geraImgGreyB_R(img, i-1, coluna);  //quando apenas j for 0, a linha i ja tera sido preenchida, e passamos para a proxima linha i-1, e voltamos para a primeira coluna->coluna
+    else geraImgGreyB_R(img, i, j-1); //quando j for diferente de 0, preenchemos a mesma linha i, e passamos para a coluna j-1
+}
