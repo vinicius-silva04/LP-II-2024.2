4f8417a87474174c0c6af0cef30145f70807fc1a
vinicius-silva04 <vjsilva.cic@uesc.br>
2024-09-06 11:11:21 -0300

    Update exemimg.c

--- LE_02/exemimg.c
+++ LE_02/exemimg.c
@@ -1,3 +1,4 @@
+#include "procimg.h"
 //Q2
 /*#include <stdio.h>
 #define linha 480
@@ -53,3 +54,26 @@ int main(){
 }
 */
 
+//Q4
+/*#include <stdio.h>
+
+int main(){
+    unsigned char img[480][640];
+    int cont = 0,i,j;
+
+    geraImgGreyW_R(img,0,0);
+    for(i = 0; i < 480; i++){                                         // Verifica se a imagem foi gerada corretamente
+        for(j = 0; j < 640; j++){ 
+            if(img[i][j] == 255){
+            cont++;                                        // Conta a quantidade de pixels gerados iguais a 255      
+            }else (printf("Erro! img[%d][%d]: %u",i,j,img[i][j]));
+        }
+    }
+    if(480*640 == cont) printf("\n\nImagem gerada com sucesso!\n");     //linha * coluna representa a quantidade de pixels da imagem, que deve ser igual a cont
+    else printf("\n\nErro na geracao da imagem!\n");
+
+
+    
+return 0;
+}
+*/
