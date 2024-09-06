ea285c7df6b9c810c20d8532c69f4db9c55f05a8
vinicius-silva04 <vjsilva.cic@uesc.br>
2024-09-06 11:12:29 -0300

    Update procimg.c

--- LE_02/procimg.c
+++ LE_02/procimg.c
@@ -12,3 +12,12 @@ void geraImgGreyB_R(unsigned char img[linha][coluna],int i ,int j){
     if(j == 0) geraImgGreyB_R(img, i-1, coluna);  //quando apenas j for 0, a linha i ja tera sido preenchida, e passamos para a proxima linha i-1, e voltamos para a primeira coluna->coluna
     else geraImgGreyB_R(img, i, j-1); //quando j for diferente de 0, preenchemos a mesma linha i, e passamos para a coluna j-1
 }
+
+//Q4
+void geraImgGreyW_R(unsigned char img[480][640],int i,int j){
+   if (i>=480 || j>=640) return;  // Condição de parada
+
+    img[i][j] = 255; // Preenche o pixel atual com 255
+     geraImgGreyW_R(img,i,j+1);  // Vai para o próximo pixel da linha
+    if(j==0) geraImgGreyW_R(img,i+1,0);  // Vai para a próxima linha
+}
