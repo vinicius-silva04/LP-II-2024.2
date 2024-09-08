3d2cd9b4872f243d15c521a2fcbb64febe56c23d
Cauan Mendonça Valadão <167363904+CauanValadao@users.noreply.github.com>
2024-09-08 16:52:04 -0300

    atualizando a 2 e 3

--- LE_02/procimg.h
+++ LE_02/procimg.h
@@ -16,21 +16,19 @@ unsigned char geraGreyPixel (int tipo);
    * tipo: determina a probabilidade dos números aleatórios.
 */
 //Q2
-void geraImgGreyFull_R(unsigned char img[linha][coluna], unsigned char pixel, int i, int j);
+void geraImgGreyFull_R(unsigned char img[linha][coluna], unsigned char pixel);
 /*
     *Função que preenche uma imagem 640x480 com pixel utilizando de recursividade
     *Parâmetro:
     * img: matriz onde a imagem será preenchida.
     *pixel: número que será preenchido a imagem.
-    *i e j: variáveis para iterar entre as linhas e colunas da matriz
 */
 //Q3
-void geraImgGreyB_R(unsigned char img[linha][coluna],int i,int j);
+void geraImgGreyB_R(unsigned char img[linha][coluna]);
 /*
     *Função que preenche uma imagem 640x480 com 0 utilizando de recursividade
     *Parâmetro:
     * img: matriz onde a imagem será preenchida.
-    *i e j: variáveis para iterar entre as linhas e colunas da matriz
 */
 //Q4
 void preencheimg(unsigned char img[480][640],int pixel,int i,int j);
