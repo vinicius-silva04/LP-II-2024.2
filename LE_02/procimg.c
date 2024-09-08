a5c4692a26f10bc8f62e9e8e73fce7960c6b1a9e
Cauan Mendonça Valadão <167363904+CauanValadao@users.noreply.github.com>
2024-09-08 16:46:37 -0300

    Update procimg.c

--- LE_02/procimg.c
+++ LE_02/procimg.c
@@ -34,11 +34,16 @@ void geraLinhaR(unsigned char array [], int tipo) {
     geraLinhaR (array, tipo);
 }
 //Q2
-void geraImgGreyFull_R(unsigned char img[linha][coluna], unsigned char pixel, int i, int j){
-    if(i == 0 || j == 0) return;    //quando i e j forem iguais a 0, a imagem estará completa, e a função encerra
-    img[i-1][j-1] = pixel;          //atribui o valor do pixel na posição i-1 e j-1
-    if(j == 0) geraImgGreyFull_R(img, pixel, i-1, coluna);  //quando apenas j for 0, a linha i ja tera sido preenchida, e passamos para a proxima linha i-1, e voltamos para a primeira coluna->coluna
-    else geraImgGreyFull_R(img, pixel, i, j-1); //quando j for diferente de 0, preenchemos a mesma linha i, e passamos para a coluna j-1
+int num = linha * coluna
+
+void geraImgGreyFull_R(unsigned char img[linha][coluna], unsigned char pixel){
+    if(num == 0){                     //testa se N chegou a 0
+        num = linha * coluna;          //reseta N para que a função possa ser chamada novamente
+        return;                      //encerra a função
+    }
+    num--;                            //decrementa N para controlar a quantidade de vezes que a função foi chamada
+    img[0][0] = pixel;              //atribui o valor 0 ao pixel atual
+    geraImgGreyFull_R(&img[0][1], pixel);     //chama a função novamente, passando o endereço do próximo pixel
 }
 //Q3
 void geraImgGreyB_R(unsigned char img[linha][coluna],int i ,int j){
