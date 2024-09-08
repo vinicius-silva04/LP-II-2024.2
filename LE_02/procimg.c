7a2738dc99165d0d4e8186aed32554119d3be721
vinicius-silva04 <vjsilva.cic@uesc.br>
2024-09-08 13:09:02 -0300

    Update procimg.c

--- LE_02/procimg.c
+++ LE_02/procimg.c
@@ -35,7 +35,7 @@ void geraLinhaR(unsigned char array [], int tipo) {
 }
 //Q2
 void geraImgGreyFull_R(unsigned char img[linha][coluna], unsigned char pixel, int i, int j){
-    if(i == 0 && j == 0) return;    //quando i e j forem iguais a 0, a imagem estará completa, e a função encerra
+    if(i == 0 || j == 0) return;    //quando i e j forem iguais a 0, a imagem estará completa, e a função encerra
     img[i-1][j-1] = pixel;          //atribui o valor do pixel na posição i-1 e j-1
     if(j == 0) geraImgGreyFull_R(img, pixel, i-1, coluna);  //quando apenas j for 0, a linha i ja tera sido preenchida, e passamos para a proxima linha i-1, e voltamos para a primeira coluna->coluna
     else geraImgGreyFull_R(img, pixel, i, j-1); //quando j for diferente de 0, preenchemos a mesma linha i, e passamos para a coluna j-1
@@ -53,16 +53,16 @@ void preencherimg(unsigned char img[480][640],int pixel,int i,int j){
     if(i>=480 || j>=640) return; //condição de parada
 
     img[i][j]=pixel; //preenche com o valor inserido
-    preencherimg(img,255,i,j+1); //preenche até o valor máximo de j
-    if(j==0)   preencherimg(img,255,i+1,0) //quando j chega no limite, vai para próxima linha
+    preencherimg(img,pixel,i,j+1); //preenche até o valor máximo de j
+    if(j==0)   preencherimg(img,pixel,i+1,0); //quando j chega no limite, vai para próxima linha
 }
-void geraImgGreyW_R(unsigned char img[480][640],int i,int j){
+void geraImgGreyW_R(unsigned char img[480][640]){
     preencherimg(img,255,0,0);
 }
 //Q5
 void geraImgGrey_R(unsigned char img[480][640],int tipo){
     unsigned char k= geraGreyPixel(tipo);
-    void preencherimg(img,k);
+    preencherimg(img,k,0,0);
 }
 //Q8
 int flag8 = -1; //usei flag = -1 para na primeira iteração flag ser igual a 0, pra ficar igual ao primeiro elemento dos arrays
