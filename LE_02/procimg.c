16717c9dba6069512b46a89cf7f10028827820c0
PedroSMagal <145768684+PedroSMagal@users.noreply.github.com>
2024-09-08 15:45:14 -0300

    Update procimg.c
    Questão 12 adicionada

--- LE_02/procimg.c
+++ LE_02/procimg.c
@@ -163,7 +163,7 @@ int pixelMin_R(unsigned char img[linha][coluna])//Entrada da matriz
             return aux_pixelMin_R(img, row, col + 1, minValue); // Chama a função auxiliar novamente, mas agora atualizando o valor da coluna, adicionando +1.
         }
     }
-    return aux_pixelMin_R(img, 0, 0, 255);// Inicializa a função auxliar, dentro da função principal, com os valores row(0), col(0), maxValue(0). Para que o laço de repetição seja feito da esquerda para a direita e que o minValue seja o maior possível(255).
+    return aux_pixelMin_R(img, 0, 0, 255);// Inicializa a função auxliar, dentro da função principal, com os valores row(0), col(0), minValue(0). Para que o laço de repetição seja feito da esquerda para a direita e que o minValue seja o maior possível(255).
 }
 //Q8
 int flag8 = -1; //usei flag = -1 para na primeira iteração flag ser igual a 0, pra ficar igual ao primeiro elemento dos arrays
@@ -190,4 +190,63 @@ void somaPorColunas_R(unsigned char img[linha][coluna], int soma[]){
     //A posição do ponteiro soma só deve mudar quando a coluna mudar
     soma[flag9 / coluna] += img[flag9 % linha][flag9 / coluna]; //Realiza a soma em cada posição do vetor soma
     somaPorColunas_R(img, soma);   //Pula para a próxima iteração
+}
+//Q12
+int quantosPixelsAbaixoDeInt_R(unsigned char img[linha][coluna], unsigned char Int)
+/*
+    *Função principal:
+    *Parametros:
+        *img: entrada da matriz.
+        *Int: entrada do valor que deve ser comparado para saber quantos valores são menores que ele.
+*/
+{
+    int aux_quantosPixelsAbaixoDeInt_R(unsigned char img[linha][coluna], unsigned char Int, int row, int col, int soma)
+    /*
+        *Função auxiliar que recebe os parametros para fazer a função principal funcionar.
+        *Parametros:
+            *img: matriz onde a imagem será preencida.
+            *Int: valor recebido para servir de base de comparação.
+            *row: parametro que representa as linhas da matriz, e será constantemente atualizada, para ter o controle da recursividade.
+            *col: parametro que representa as colunas da matriz, e será constantemente atualizada, para ter o controle da recursividade.
+            *soma: Para somar quantos valores são menores que o parametro Int.
+
+    */
+    {
+        if(row == 480)
+        {
+            return soma;
+        }
+        /*
+           IF para verificar a condição de saída da recursividade, nesse caso, o método de saída será quando row(linha) for igual a 480, pois a matriz vai de 0 a 479, se chegar em 480
+           irá passar do tamanho da matriz.
+
+           Quando o caso for positivo, então ele deve retornar o valor soma, que foi modificado ao decorrer do código para ter a soma da quantidade dos valores menores que Int da matriz.
+        */
+
+        if(col > 639)
+        {
+            return aux_quantosPixelsAbaixoDeInt_R(img, Int, row + 1, 0, soma);
+        }
+        /*
+            IF para verficar se a coluna(col) passou de 639, que é o valor máximo que uma coluna pode ter nessa matriz.
+
+            Quando col == 640, ele irá chamar a função novamente chamando a matriz img, mas agora a row(linha) será adicionada +1, portanto a próxima linha. A col receberá o valor 0, para
+            começar a repetição novamente, e o valor soma e Int irá passar para a próxima função a ser chamada.
+        */
+        else // ELSE para caso col seja menor que 639, ocorrerá na maioria das vezes
+        {
+            if(img[row][col] < Int)
+            {
+                soma ++;
+            }
+            /* 
+                IF para verificar se o valor da matriz img de posição [linha][coluna], row e col respectivamente, é menor que o valor base Int.
+                
+                Caso o IF seja positivo soma recebe o valor adicionado +1.
+            */
+            return aux_quantosPixelsAbaixoDeInt_R(img, Int, row, col + 1, soma); // Chama a função auxiliar novamente, mas agora atualizando o valor da coluna, adicionando +1.
+        }
+    }
+
+    return aux_quantosPixelsAbaixoDeInt_R(img, Int, 0, 0, 0);// Inicializa a função auxliar, dentro da função principal, com os valores row(0), col(0), soma(0). Para que o laço de repetição seja feito da esquerda para a direita.
 }
