eb411e149c49d24890cb8cb6defae6da4ea72c36
Cauan Mendonça Valadão <167363904+CauanValadao@users.noreply.github.com>
2024-09-08 16:54:09 -0300

    Update exemimg.c

--- LE_02/exemimg.c
+++ LE_02/exemimg.c
@@ -26,49 +26,45 @@ int main () {
 //Q2
 /*
 #include <stdio.h>
-#define linha 480
-#define coluna 640
 int main(){
-    unsigned char img[linha][coluna], pixel;
-    int cont = 0;
-
+    unsigned char img[linha][coluna] = {0}, pixel;
+    int i, j, soma = 0;
+    
     printf("Digite o valor do pixel: ");
     scanf("%hhu", &pixel);
-    
-    geraImgGreyFull_R(img, pixel, 480, 640);
-    
-    for(int i = 0; i < linha; i++){                                         // Verifica se a imagem foi gerada corretamente
-        for(int j = 0; j < coluna; j++){ 
-            if(img[i][j] != pixel){
-                printf("Erro: img[%d][%d] = %u\n", i, j, img[i][j]);          // Se algum pixel não for igual ao valor digitado, a imagem não foi gerada corretamente
-                return 0;
-            }
-            else cont++;                                        // Conta a quantidade de pixels gerados iguais ao valor digitado         
+
+    geraImgGreyFull_R(img, pixel);
+
+
+    for(i = 0; i < linha; i++){                 // Verifica se a imagem foi gerada corretamente
+        for(j = 0; j < coluna; j++){
+            if(img[i][j] == pixel) soma++;
         }
-    }printf("Exemplo:\nimg[%d][%d]: %u",linha,coluna,img[linha-1][coluna-1]);
+    }
 
-    if(linha * coluna == cont) printf("\n\nImagem gerada com sucesso!\n");     //linha * coluna representa a quantidade de pixels da imagem, que deve ser igual a cont
-    else printf("\n\nErro na geração da imagem!\n");
+    if(soma != linha*coluna) printf("erro");              //se a soma for diferente de linha*coluna, a imagem não foi gerada corretamente
+    else printf("deu certo");
 
+    
     return 0;
 }
 */
 //Q3
 /*
 #include <stdio.h>
-#define linha 480
-#define coluna 640
 int main(){
     unsigned char img[linha][coluna];
     int cont = 0;
-   
-    geraImgGreyB_R(img,linha,coluna);
+
+
+    geraImgGreyB_R(img);
     
+
     for(int i = 0; i < linha; i++){                                         // Verifica se a imagem foi gerada corretamente
         for(int j = 0; j < coluna; j++){ 
             if(img[i][j] != 0){
-                printf("Erro: img[%d][%d] = %u\n", i, j, img[i][j]);          // Se algum pixel não for igual a 0, a imagem não foi gerada corretamente
-                return 0;
+                printf("Erro: img[%d][%d] = %hhu\n", i, j, img[i][j]);          // Se algum pixel não for igual a 0, a imagem não foi gerada corretamente
+             
             }
             else cont++;                                        // Conta a quantidade de pixels gerados iguais a 0        
         }
