62912431fbcd9766be02ba414394d8eb26a83758
vinicius-silva04 <vjsilva.cic@uesc.br>
2024-09-13 08:37:31 -0300

    Ctrl z se n for

--- LE_02/exemimg.c
+++ LE_02/exemimg.c
@@ -1,434 +1,244 @@
-#include "procimg.h"
-
-//Q1
-/*#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "procimg.h"  
+
+#define LINHA 480
+#define COLUNA 640
 #define N 640
 
-int main () {
-    int tipo, escuro=0, claro=0;
-    unsigned char array [N];
-    srand (time (NULL));
-    printf (" tipo < 0 = cores mais escuras\n tipo > 0 = cores mais claras\n tipo == 0 = todas as intensidades\n");
-    printf ("\nDigite o tipo de intensidade: ");
-    scanf ("%d", &tipo);
+int main() {
+    int tipo, escuro = 0, claro = 0;
+    unsigned char array[N];
+    unsigned char img[LINHA][COLUNA];
+    int i, j, soma = 0, cont = 0;
+    unsigned char pixel;
+    int pixelMax, pixelMin, min = 0, max = 0;
+    int somaparcial = 0, somaLinhas[LINHA] = {0}, somaColunas[COLUNA] = {0};
+    int somaTotal;
+    unsigned char inte;
+    int count = 0, x, maior = 0, abaixoDeInt, acimaDeInt;
+
+    srand(time(NULL));
+
+    // Q1 - GeraLinhaR
+    printf("\nQuestao 1\n");
+    printf("Tipo < 0 = cores mais escuras\nTipo > 0 = cores mais claras\nTipo == 0 = todas as intensidades\n");
+    printf("\nDigite o tipo de intensidade: ");
+    scanf("%d", &tipo);
     geraLinhaR(array, tipo);         
-    for (int i = 0; i < N; i++) {
-        if (array[i]<128) escuro++;
-        if (array[i]>=128) claro++;
+    for (i = 0; i < N; i++) {
+        if (array[i] < 128) escuro++;
+        if (array[i] >= 128) claro++;
     }
-    if (tipo<0 && escuro>claro || tipo>0 && claro>escuro || tipo==0)
-        printf ("\nArray preenchido corretamente!\n");
+    if (tipo < 0 && escuro > claro || tipo > 0 && claro > escuro || tipo == 0)
+        printf("\nArray preenchido corretamente!\n");
     else
-        printf ("\nErro: O array nao foi preenchido majoritariamente pelo tipo solicitado.\n");
-    printf ("Cores escuras: %d\nCores claras: %d\n", escuro, claro);
-    return 0;
-}
-*/
-//Q2
-/*
-#include <stdio.h>
-int main(){
-    unsigned char img[linha][coluna] = {0}, pixel;
-    int i, j, soma = 0;
-    
+        printf("\nErro: O array nao foi preenchido majoritariamente pelo tipo solicitado.\n");
+    printf("Cores escuras: %d\nCores claras: %d\n", escuro, claro);
+
+    // Q2 - GeraImgGreyFull_R
+    printf("\nQuestao 2\n");
     printf("Digite o valor do pixel: ");
     scanf("%hhu", &pixel);
-
     geraImgGreyFull_R(img, pixel);
-
-
-    for(i = 0; i < linha; i++){                 // Verifica se a imagem foi gerada corretamente
-        for(j = 0; j < coluna; j++){
+    soma = 0;
+    for(i = 0; i < LINHA; i++) {
+        for(j = 0; j < COLUNA; j++) {
             if(img[i][j] == pixel) soma++;
         }
     }
+    if(soma != LINHA * COLUNA) printf("Erro na geracao da imagem full grey\n");
+    else printf("Imagem grey full gerada corretamente\n");
 
-    if(soma != linha*coluna) printf("erro");              //se a soma for diferente de linha*coluna, a imagem não foi gerada corretamente
-    else printf("deu certo");
-
-    
-    return 0;
-}
-*/
-//Q3
-/*
-#include <stdio.h>
-int main(){
-    unsigned char img[linha][coluna];
-    int cont = 0;
-
-
+    // Q3 - GeraImgGreyB_R
+    printf("\nQuestao 3\n");
     geraImgGreyB_R(img);
-    
-
-    for(int i = 0; i < linha; i++){                                         // Verifica se a imagem foi gerada corretamente
-        for(int j = 0; j < coluna; j++){ 
-            if(img[i][j] != 0){
-                printf("Erro: img[%d][%d] = %hhu\n", i, j, img[i][j]);          // Se algum pixel não for igual a 0, a imagem não foi gerada corretamente
-             
-            }
-            else cont++;                                        // Conta a quantidade de pixels gerados iguais a 0        
+    cont = 0;
+    for(i = 0; i < LINHA; i++) {
+        for(j = 0; j < COLUNA; j++) {
+            if(img[i][j] != 0) {
+                printf("Erro: img[%d][%d] = %hhu\n", i, j, img[i][j]);
+            } else cont++;
         }
     }
+    if(LINHA * COLUNA == cont) printf("Imagem grey B gerada com sucesso!\n");
+    else printf("Erro na geracao da imagem grey B!\n");
 
-    if(linha * coluna == cont) printf("\n\nImagem gerada com sucesso!\n");     //linha * coluna representa a quantidade de pixels da imagem, que deve ser igual a cont
-    else printf("\n\nErro na geracao da imagem!\n");
-
-    return 0;
-}
-*/
-//Q4
-/*#include <stdio.h>
-
-int main(){
-    unsigned char img[480][640];
-    int cont = 0,i,j;
-
+    // Q4 - GeraImgGreyW_R
+    printf("\nQuestao 4\n");
     geraImgGreyW_R(img);
-    for(i = 0; i < 480; i++){                                         // Verifica se a imagem foi gerada corretamente
-        for(j = 0; j < 640; j++){ 
-            if(img[i][j] == 255){
-            cont++;                                        // Conta a quantidade de pixels gerados iguais a 255      
-            }else{
-                printf("Erro! img[%d][%d]: %u",i,j,img[i][j]);
+    cont = 0;
+    for(i = 0; i < LINHA; i++) {
+        for(j = 0; j < COLUNA; j++) {
+            if(img[i][j] == 255) {
+                cont++;
+            } else {
+                printf("Erro! img[%d][%d]: %u\n", i, j, img[i][j]);
                 return 0;
             }
         }
-    }printf("Exemplo:\nimg[%d][%d]: %u",i,j,img[i-1][j-1]);
-    if(480*640 == cont) printf("\n\nImagem gerada com sucesso!\n");     //linha * coluna representa a quantidade de pixels da imagem, que deve ser igual a cont
-    else printf("\n\nErro na geracao da imagem!\n");
+    }
+    printf("Exemplo:\nimg[%d][%d]: %u\n", i-1, j-1, img[i-1][j-1]);
+    if(LINHA * COLUNA == cont) printf("Imagem grey W gerada com sucesso!\n");
+    else printf("Erro na geracao da imagem grey W!\n");
 
-
-    
-return 0;
-}
-*/
-//Q5
-/*
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-int main(){
-    unsigned char img[linha][coluna];
-    int i, j, tipo=0;
-    srand(time(NULL));
+    // Q5 - GeraImgGrey_R
+    printf("\nQuestao 5\n");
     geraImgGrey_R(img, tipo);
-    for(i = 0; i < linha; i++){
-        for(j = 0; j < coluna; j++){
+    for(i = 0; i < LINHA; i++) {
+        for(j = 0; j < COLUNA; j++) {
             int k = img[i][j];
-            if (k<0 || k>255){
-                printf("Erro na imagem gerada!\nimg[%d][%d]: %hhu",i, j, img[i][j]);
+            if (k < 0 || k > 255) {
+                printf("Erro na imagem gerada!\nimg[%d][%d]: %hhu\n", i, j, img[i][j]);
                 return 1;
             }
         }
     }
-    printf("Exemplo:\nimg[%d][%d] = %hhu", i-1, j-1, img[i-1][j-1]);
-    printf("\n\nImagem gerada com sucesso\n");
-    return 0;
-}
-*/
-//Q6
-/*
-#include <stdio.h>
+    printf("Exemplo:\nimg[%d][%d] = %hhu\n", i-1, j-1, img[i-1][j-1]);
+    printf("Imagem grey gerada com sucesso\n");
 
-int main(void)
-{
-    unsigned char img[linha][coluna];
-    int i, j, pixelMax, max = 0;
-    // Iniciando a semente para gerar números aleatórios diferentes a cada execução
-    srand(time(NULL));
-
-    // Preenchendo a matriz img com valores aleatórios entre 0 e 255
-    for (i = 0; i < linha; i++)
-    {
-        for (j = 0; j < coluna; j++)
-        {
+    // Q6 - pixelMax_R
+    printf("\nQuestao 6\n");
+    for (i = 0; i < LINHA; i++) {
+        for (j = 0; j < COLUNA; j++) {
             img[i][j] = rand() % 256;
         }
     }
-
-    pixelMax = pixelMax_R(img); // Coloca o valor da função pixelMax_R na variavel pixelMax
-    
-    printf("O maior valor eh %d", pixelMax);
-
-    for(i = 0; i<linha; i++){
-        for(j = 0; j<coluna; j++){
-            if(img[i][j] > max){
+    pixelMax = pixelMax_R(img); 
+    printf("O maior valor eh %d\n", pixelMax);
+    max = 0;
+    for(i = 0; i < LINHA; i++) {
+        for(j = 0; j < COLUNA; j++) {
+            if(img[i][j] > max) {
                 max = img[i][j];
             }
         }
     }
-    if(max == pixelMax) printf("\nTudo certo\n");
-    else printf("\nErro\n");
+    if(max == pixelMax) printf("Maximo correto\n");
+    else printf("Erro no calculo do maximo\n");
 
-    return 0;
-}
-*/
-//Q7
-/*
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-
-int main(void)
-{
-    unsigned char img[linha][coluna];
-    int i, j, pixelMin, min;
-    // Iniciando a semente para gerar números aleatórios diferentes a cada execução
-    srand(time(NULL));
-
-    // Preenchendo a matriz img com valores aleatórios entre 0 e 255
-    for (i = 0; i < linha; i++)
-    {
-        for (j = 0; j < coluna; j++)
-        {
+    // Q7 - pixelMin_R
+    printf("\nQuestao 7\n");
+    for (i = 0; i < LINHA; i++) {
+        for (j = 0; j < COLUNA; j++) {
             img[i][j] = rand() % 256;
         }
     }
-
-    pixelMin = pixelMin_R(img); // Coloca o valor da função pixelMax_R na variavel pixelMax
-
-    printf("O menor valor eh %d", pixelMin);
-
-    for(i = 0; i< linha; i++){
-        for(j = 0; j< coluna; j++){
-            if(img[i][j] < min){
+    pixelMin = pixelMin_R(img);
+    printf("O menor valor eh %d\n", pixelMin);
+    min = 255;
+    for(i = 0; i < LINHA; i++) {
+        for(j = 0; j < COLUNA; j++) {
+            if(img[i][j] < min) {
                 min = img[i][j];
             }
         }
     }
+    if(min == pixelMin) printf("Minimo correto\n");
+    else printf("Erro no calculo do minimo\n");
 
-    if(min == pixelMin) printf("\nTudo certo\n");
-    else printf("\nErro\n");
-
-    return 0;
-}
-*/
-//Q8
-/*
-int main(){
-    unsigned char img[linha][coluna];
-    int soma[linha] = {0}, i = 0, j = 0, somaparcial = 0;    //somaparcial será usado para verificação de cada soma
-    srand(time(NULL));
-
-    //preenchimento da matriz
-    for(i = 0; i < linha; i++){
-        for(j = 0; j < coluna; j++){
+    // Q8 - somaPorLinhas_R
+    printf("\nQuestao 8\n");
+    for(i = 0; i < LINHA; i++) {
+        for(j = 0; j < COLUNA; j++) {
             img[i][j] = rand() % 256;
         }
     }
-
-    somaPorLinhas_R(img, soma);   //chamada da função
-
-     for(i = 0; i < linha; i++){
+    somaPorLinhas_R(img, somaLinhas);
+    for(i = 0; i < LINHA; i++) {
         somaparcial = 0;
-        for(j = 0; j < coluna; j++){
-            somaparcial += img[i][j];   //Verificação de cada soma
+        for(j = 0; j < COLUNA; j++) {
+            somaparcial += img[i][j];
         }
-        if(soma[i] != somaparcial){
-            printf("Array soma = %d, soma parcial = %d", soma[i], somaparcial);  //Mostra o erro
-            printf("ERRO!!");   //Verificação, se a soma parcial for diferente da soma no respectivo local do array, é erro.
+        if(somaLinhas[i] != somaparcial) {
+            printf("Erro! Array soma = %d, soma parcial = %d\n", somaLinhas[i], somaparcial);
             return 0;
         }
-        
     }
-    
-    //Mostra a ultima soma parcial e ultima soma do vetor
-    printf("ultima soma do array = %d, ultima soma parcial = %d", soma[linha - 1], somaparcial); 
-    printf("\nTUDO CERTO!!\n");
+    printf("Soma por linhas correta\n");
 
-    return 0;
-}
-*/
-//Q9
-/*
-int main(){
-    unsigned char img[linha][coluna] = {0};
-    int i = 0, j = 0, soma[coluna] = {0}, somaparcial = 0;   
-    srand(time(NULL));
-
-    //preenchimento da matriz
-     for(i = 0; i < linha; i++){
-        for(j = 0; j < coluna; j++){
+    // Q9 - somaPorColunas_R
+    printf("\nQuestao 9\n");
+    for(i = 0; i < LINHA; i++) {
+        for(j = 0; j < COLUNA; j++) {
             img[i][j] = rand() % 256;
         }
     }
-    
-    somaPorColunas_R(img, soma);
-    for(j = 0; j < coluna; j++){
+    somaPorColunas_R(img, somaColunas);
+    for(j = 0; j < COLUNA; j++) {
         somaparcial = 0;
-        for(i = 0; i < linha; i++){
-            somaparcial += img[i][j];   //somaparcial será usado para verificação de cada soma
+        for(i = 0; i < LINHA; i++) {
+            somaparcial += img[i][j];
         }
-        if(soma[j] != somaparcial){
-            printf("Array soma = %d, soma parcial = %d", soma[j], somaparcial);  // Mostra o erro
-            printf("\nERRO!!\n");   //Verificação, se a soma parcial for diferente da soma no respectivo local do array, é erro.
+        if(somaColunas[j] != somaparcial) {
+            printf("Erro! Array soma = %d, soma parcial = %d\n", somaColunas[j], somaparcial);
             return 0;
         }
     }
+    printf("Soma por colunas correta\n");
 
-    //Mostra a ultima soma parcial e ultima soma do vetor
-    printf("ultima soma array = %d, ultima soma parcial = %d\n", soma[coluna - 1], somaparcial); 
-    printf("\nTUDO CERTO!!\n");
-        
-    return 0;
-}
-*/
-//Q10
-/*
-int main() {
-    unsigned char img[linha][coluna];
-    int soma;
-
-    // Preenche a matriz img com o valor 1 em todas as posições
-    for (int i = 0; i < linha; i++) {
-        for (int j = 0; j < coluna; j++) {
+    // Q10 - somaPorTotal_R
+    printf("\nQuestao 10");
+    for (i = 0; i < LINHA; i++) {
+        for (j = 0; j < COLUNA; j++) {
             img[i][j] = 1;
         }
     }
-
-    // Calcula a soma dos elementos da matriz usando a função recursiva
-    soma = somaPorTotal_R(img);
-
-    // Verifica se a soma calculada é igual ao número total de elementos (linha * coluna)
-    if (soma == linha * coluna)
-        printf("Soma total: %d\n", soma);
+    somaTotal = somaPorTotal_R(img);
+    if (somaTotal == LINHA * COLUNA)
+        printf("Soma total correta: %d\n", somaTotal);
     else
-        printf("Erro: soma incorreta.\n");
+        printf("Erro na soma total\n");
 
-    return 0;
-}
-*/
-//Q11
-/*
-#include <stdio.h>
-
-int main () {
-    unsigned char img [linha][coluna], inte;
-    int i, j, count=0;
-    srand (time (NULL));
-    for (i = 0; i<linha; i++) {
-        for (j = 0; j<coluna; j++) {
-            img [i][j] = rand () % 256;     //preencher a img com numeros aleatorios
-        }
-    }
-    printf ("Digite o numero que quer encontrar: ");
-    scanf ("%hhu", &inte);      //pede para o usuário qual intensidade ele quer procurar
-    
-    for (i = 0; i<linha; i++) {
-        for (j = 0; j<coluna; j++) {
-            if (img[i][j]==inte) count++;   //contador de frequência do inte na img
-        }
-    }
-    if (count==quantosPixelsNaInt_R (img, inte))   //verifica se a função retorna a contagem correta
-        printf ("\nO numero %hhu apareceu %d vezes \n", inte, count);   //imprime a frequência se estiver correta
-    else
-        printf ("\nErro: Contagem incorreta.\n");  // exibe mensagem de erro se estiver incorreta
-}
-*/
-//Q12
-/*
-int main(void)
-{
-    unsigned char img[linha][coluna];
-    int i, j, min = 0;
-    // Iniciando a semente para gerar números aleatórios diferentes a cada execução
-    srand(time(NULL));
-
-    // Preenchendo a matriz img com valores aleatórios entre 0 e 255
-    for (i = 0; i < linha; i++)
-    {
-        for (j = 0; j < coluna; j++)
-        {
+    // Q11 - quantosPixelsNaInt_R
+    printf("\nQuestao 11");
+    for (i = 0; i < LINHA; i++) {
+        for (j = 0; j < COLUNA; j++) {
             img[i][j] = rand() % 256;
         }
     }
-
-    int x = quantosPixelsAbaixoDeInt_R(img, 20);
-    printf("Teve %d valores menor que o valor dado no parametro", x);
-
-    for(i = 0; i < linha; i++){
-        for(j = 0; j < coluna; j++){
-            if(img[i][j] < 20) min++;
+    printf("Digite o numero que quer encontrar: ");
+    scanf("%hhu", &inte);
+    count = 0;
+    for (i = 0; i < LINHA; i++) {
+        for (j = 0; j < COLUNA; j++) {
+            if (img[i][j] == inte)
+                count++;
         }
     }
+    x = quantosPixelsNaInt_R(img, inte);
+    printf("Quantidade do numero %d eh %d\n", inte, x);
+    if (x == count) printf("Quantidade correta\n");
+    else printf("Quantidade incorreta\n");
 
-    if(min == x) printf("\nTudo certo\n");
-    else printf("\nErro\n");
+    // Q12 - quantosPixelsAbaixoDeInt_R
+    printf("\nQuestao 12\n");
+    printf("Digite o numero: ");
+    scanf("%hhu", &inte);
+    abaixoDeInt = quantosPixelsAbaixoDeInt_R(img, inte);
+    count = 0;
+    for(i = 0; i < LINHA; i++) {
+        for(j = 0; j < COLUNA; j++) {
+            if(img[i][j] < inte) count++;
+        }
+    }
+    if(count == abaixoDeInt) printf("\nResultado correto\n");
+    else printf("\nResultado incorreto\n");
+
+    // Q13 - quantosPixelsAcimaDeInt_R
+    printf("\nQuestao 13\n");
+    printf("\nDigite o numero: ");
+    scanf("%hhu", &inte);
+    acimaDeInt = quantosPixelsAcimaDeInt_R(img, inte);
+    count = 0;
+    for(i = 0; i < LINHA; i++) {
+        for(j = 0; j < COLUNA; j++) {
+            if(img[i][j] > inte) count++;
+        }
+    }
+    if(count == acimaDeInt) printf("\nResultado correto\n");
+    else printf("\nResultado incorreto\n");
 
     return 0;
 }
-*/
-
-//Q13
-/*
-int main(){
-    unsigned char img[linha][coluna];
-    int i, j, x, maior = 0, acima;
-    srand(time(NULL));
-
-    for(i = 0; i < linha; i++) {
-        for(j = 0; j < coluna; j++) {
-            img[i][j] = rand() % 256;
-        }
-    }
-    printf("Digite um valor inteiro: ");
-    scanf("%d", &x);
-
-    acima = quantosPixelsAcimaDeInt_R(img, x);
-
-    for(i = 0; i < linha; i++) {
-        for(j = 0; j < coluna; j++) {
-            if(img[i][j] > x) maior++;
-        }
-    }
-    if(maior == acima) printf("A funcao esta correta\n");
-    else printf("A funcao esta incorreta\n");
-
-    return 0;
-}
-*/
-//Q14
-/*
-int main(){
-    unsigned char img[linha][coluna], Int = 0, equilibrio;
-    int i, j, menorint, maiorint, media, reserva;
-    srand(time(NULL));
-
-    //preenchimento da matriz
-     for(i = 0; i < linha; i++){
-        for(j = 0; j < coluna; j++){
-            img[i][j] = rand() % 256;
-        }
-    }
-
-    menorint = pixelMin_R(img);
-    maiorint = pixelMax_R(img);
-    media = (menorint + maiorint)/2;
-
-    equilibrio = pontoEquilibrio(img, Int);
-
-    Int = 0;
-    for(i = 0; i < linha; i++){
-        for(j = 0; j < coluna; j++){
-            reserva = img[i][j];
-            if(reserva == media){
-                Int = reserva;    //Verificação do valor mais próximo da média
-                break;
-            }
-            else if(abs(reserva - media) < abs(Int - media)){
-                Int = reserva;    //Verificação do valor mais próximo da média
-            }
-        }
-    }
-
-    if(Int != equilibrio){
-        printf("Int = %d, equilibrio = %d\nERRO!!!\n", Int, equilibrio);   //Se for diferente lascou
-    }
-    return 0;
-}
-*/
