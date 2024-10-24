33e926b3a3b838b19db05f953cf2fc771a5bcb39
Cauan Mendonça Valadão <167363904+CauanValadao@users.noreply.github.com>
2024-10-24 14:52:55 -0300

    Update procimg.h

--- LE_05/procimg.h
+++ LE_05/procimg.h
@@ -1,3 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef unsigned char uchar;
+
+typedef struct {
+    uchar *_img;
+    uchar **img;
+    int nLin,nCol;
+
+}imgGray;
+
+typedef struct{
+    uchar R,G,B;
+}tRGB;
+
+typedef struct{
+    tRGB *_img;
+    tRGB **img;
+    int nLin,nCol;
+}imgRGB;
+
+struct imagens{
+    imgGray img;
+    struct imagens *prox; 
+};
+typedef struct imagens tImagens;
+
 #include "procimg.c"
 
 //Função free imgGray
