cb4b9a7c7727cff875e64b491321f5708a9a9bbc
vinicius-silva04 <vjsilva.cic@uesc.br>
2024-10-23 23:19:44 -0300

    Update procimg.h

--- LE_05/procimg.h
+++ LE_05/procimg.h
@@ -3,7 +3,52 @@
 
 //Q1
 imgGray alocaImagemGray(int nLin,int nCol);
+/*
+    *Função que aloca imagem em tons de cinza
+    *Parâmetros:  
+    *nLin: quantidade de linhas da matriz
+    *nCol: quantidade de colunas da matriz  
+    *Retorno:
+    *Uma struct com a imagem alocado ou ponteiros nulos em caso de falha 
+*/
+
 //Q2
 imgRGB  alocaImagemRGB(int nLin,int nCol);
+/*
+    *Função que aloca imagem colorida
+    *Parâmetros:
+    *nLin: quantidade de linhas da matriz
+    *nCol: quantidade de colunas da matriz
+    *Retorno:
+    *Uma struct com a imagem alocado ou ponteiros nulos em caso de falha
+*/
+
 //Q3
 int geraImgGrayFull(imgGray img,uchar pixel);
+/*
+    *Função que preenche todos os pixels com o valor de pixel 
+    *Parâmetros:
+    *img: imagem já alocada
+    *pixel: valor que preencherá a imagem
+    *Retorno:
+    *True(1) se funcionar corretamente,False(0) se houver algum erro
+*/
+//Q4
+int geraImgRGBFull(imgRGB img,tRGB pixel);
+/*
+    *Função que preenche todos os pixels com o valor de pixel 
+    *Parâmetros:
+    *img: imagem já alocada previamente 
+    *pixel: valor da cor RGB para preencher todos os pixels da imagem.
+    *Retorno:
+    *True(1) se funcionar corretamente,False(0) se houver algum erro
+*/
+//Q5 
+int geraImgGrayB(imgGray img);
+/*
+    *Função que preenche todos os pixels com 0
+    *Parâmetro:
+    *img: Imagem já alocada previamente
+    *Retorno:
+    *True(1) se funcionar corretamente,False(0)d se houver algum erro
+*/
