a0c09003c8dd4992fa495c489a074753d68a29d1
Cauan Mendonça Valadão <167363904+CauanValadao@users.noreply.github.com>
2024-10-24 14:47:35 -0300

    6-10

--- LE_05/exeming.c
+++ LE_05/exeming.c
@@ -220,3 +220,87 @@ int main() {
 
     return 0;
 }
+
+    //funcoes 6-10
+    imgGray imagemC;
+    imgRGB imagemCrgb;
+    int teste = 0;
+    imagemC = alocaImagemGray(10,10);
+    imagemCrgb = alocaImagemRGB(30,30);
+
+    //funcao 6
+    geraImgRGBB(imagemCrgb);
+
+    teste = 0;
+
+    for(i = 0; i < imagemCrgb.nCol*imagemCrgb.nLin; i++) if(imagemCrgb._img[i].R || imagemCrgb._img[i].G || imagemCrgb._img[i].B) teste = 1;
+
+    if(teste)
+        printf("\nExecucao da funcao 6 falhou");
+    else
+        printf("\nExecucao da funcao 6 teve exito");
+
+
+    //funcao 7
+    geraImgGrayW(imagemC);
+
+    teste = 0;
+    for(i = 0; i < imagemC.nCol*imagemC.nLin; i++) if(imagemC._img[i] != 255) teste = 1;
+    if(teste)
+        printf("\nExecucao da funcao 7 falhou");
+    else
+        printf("\nExecucao da funcao 7 teve exito");
+
+    //funcao 8
+
+    geraImgRGBW(imagemCrgb);
+
+    teste = 0;
+    for(i = 0; i < imagemCrgb.nCol*imagemCrgb.nLin; i++) if(imagemCrgb._img[i].R != 255 || imagemCrgb._img[i].G != 255 || imagemCrgb._img[i].B != 255) teste = 1;
+    if(teste)
+        printf("\nExecucao da funcao 8 falhou");
+    else
+        printf("\nExecucao da funcao 8 teve exito");
+    
+    //funcao 9
+    geraImgGray(imagemC, 1);
+    int maior = 0, menor = 0;
+    teste = 0;
+
+    for(i = 0; i < imagemC.nCol*imagemC.nLin; i++){
+        if(imagemC._img[i] >= 128) maior++;
+        else menor++;
+    }
+    if(maior < menor) teste = 1;
+    
+    if(teste)
+        printf("\nExecucao da funcao 9 falhou");
+    else
+        printf("\nExecucao da funcao 9 teve exito");
+
+    //funcao 10
+    geraImgRGB(imagemCrgb, 1);
+    maior = 0;
+    menor = 0;
+    teste = 0;
+
+    for(i = 0; i < imagemCrgb.nCol*imagemCrgb.nLin; i++){
+        if(imagemCrgb._img[i].R >= 128) maior++;
+        else menor++;
+        if(imagemCrgb._img[i].G >= 128) maior++;
+        else menor++;
+        if(imagemCrgb._img[i].B >= 128) maior++;
+        else menor++;
+    }
+    if(maior < menor) teste = 1;
+
+    if(teste)
+        printf("\nExecucao da funcao 10 falhou");
+    else
+        printf("\nExecucao da funcao 10 teve exito");
+
+    free(imagemC._img);
+    free(imagemC.img);
+    free(imagemCrgb._img);
+    free(imagemCrgb.img);
+    free(vet);
